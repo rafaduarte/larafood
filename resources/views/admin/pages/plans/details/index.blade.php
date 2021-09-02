c02d4ccdfedb15bfafbd1897fb387734fc175106
rafaduarte <rafaelduartedelimaa@gmail.com>
2021-09-02 10:38:29 -0300

    deletar detalhes

--- resources/views/admin/pages/plans/details/index.blade.php
+++ resources/views/admin/pages/plans/details/index.blade.php
@@ -15,6 +15,7 @@
 
 @section('content')
         <div class="card-body">
+            @include('admin.includes.alerts')
                 <table class="table table-condensed">
                     <thead>
                         <tr>
@@ -30,7 +31,7 @@
                         </td>
                         <td  style="width=10px">
                             <a href="{{ route('details.plan.edit', [$plan->url, $detail->id])}}" class="">Edit</a>
-                            <a href="{{ route('plans.show', $plan->url)}}" class="btn btn-warning">Ver</a>
+                            <a href="{{ route('details.plan.show', [$plan->url, $detail->id])}}" class="btn btn-warning">Ver</a>
                         </td>
                     </tr>
                 @endforeach
